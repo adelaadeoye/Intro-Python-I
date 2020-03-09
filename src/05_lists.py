c524a4017122440893d8e1361339ad4d520330b8
adeoye adela <adeoye.adela@gmail.com>
2020-03-10 00:04:28 +0100

    Excercise 5 done

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,39 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+print(f"current Items in x: {x}")
+x.append(4)
+print(f"Updated Items in x:")
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+print(f"current Items in x: {x}")
+x.extend(y)
+print(f"Updated Items in x:")
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+print(f"current Items in x: {x}")
+x.remove(8)
+print(f"Updated Items in x:")
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+print(f"current Items in x: {x}")
+x.insert(5,99)
+print(f"Updated Items in x:")
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
-
+print(f"Length of x list is: {len(x)}")
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+def itemMul(x):
+    print(f"Multiplying each Item in x by 1000")
+    for item in x:
+        print(f"{item} *1000 =\"{item*1000}")
+itemMul(x)
