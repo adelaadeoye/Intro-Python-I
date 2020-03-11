0e023cf49d86ec91fc43cc7e567daecd4ee6ca1b
adeoye adela <adeoye.adela@gmail.com>
2020-03-11 15:19:10 +0100

    Exercise 11 done

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(a,b):
+    return (a,b)
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
@@ -13,7 +14,8 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+def f2(*argv):
+    return sum(argv)
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -22,6 +24,8 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
+def f2(a):
+   return sum(list(a)) 
 print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
@@ -30,6 +34,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(*argv):
+    if(len(argv)==1):
+        return ((argv[0])+1)
+    elif(len(argv)==2):
+        return sum(list(argv))
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,7 +53,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**argv):
+    for k,v in argv.items():
+        print(f"key: {k}, value: {v}")
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -62,4 +73,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
