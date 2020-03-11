17b3b697e8bdb866ab19fca5da11eae89f575e56
adeoye adela <adeoye.adela@gmail.com>
2020-03-11 11:31:45 +0100

    Exercise 10 done

--- src/10_functions.py
+++ src/10_functions.py
@@ -4,9 +4,17 @@
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
-num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
-
+try:
+    num = int(num)
+    if(num !=0):
+        if(num %2==0):
+            print("EVEN")
+        else:
+            print("ODD")
+    else:
+        print("Input can't be zero or null")
+except ValueError:
+    print("Only number is accepted")
